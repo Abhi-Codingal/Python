7d0599ed2cb37e21cb69dbbb510eb2bc368abfdc
Abhi-Codingal <thisisabhimanyu2.0@gmail.com>
2025-07-10 16:44:19 -0500

    initial commit

---
+++ Lesson 6/alphabet.py
@@ -0,0 +1,8 @@
+letter =input("Enter 1 character:")
+
+if letter >= "A" and letter <= "Z":
+    print(letter,"is a letter.")
+elif letter>="a" and letter<="z":
+    print(letter,"is a letter.")
+else:
+    print(letter,"is not a letter.")    
