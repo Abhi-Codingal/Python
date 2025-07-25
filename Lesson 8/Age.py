ccbc6603da9e87793e4cdd3554b13b91ff23e948
Abhi-Codingal <thisisabhimanyu2.0@gmail.com>
2025-07-25 14:35:46 -0500

    initial commit

---
+++ Lesson 8/Age.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:"))
+if age>=60:
+    print("You are a senior citizen")
+elif age>=18:
+    print("You are an adult")
+else:
+    print("You are a kid")
---
+++ Lesson 8/Test.py
@@ -0,0 +1,5 @@
+score=int(input("Enter your test score out of a hundred:"))
+if score>=80:
+    print("You passed the test")
+else:
+    print("You failed the test")
