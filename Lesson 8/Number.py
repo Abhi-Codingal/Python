c931627b9f591d7c90545ba93d6cac2e7051197d
Abhi-Codingal <thisisabhimanyu2.0@gmail.com>
2025-07-25 14:40:32 -0500

    inintial commit

--- Lesson 8/Number.py
+++ Lesson 8/Number.py
@@ -1,7 +1,10 @@
 num1=int(input("Enter a number:"))
 num2=int(input("Enter another number:"))
+num3=int(input("Enter a third number:"))
+num4=num3
 num3=num2
 num2=num1
-num1=num3
+num1=num4
 print("First number now equals:",num1)
 print("Second number now equals:",num2)
+print("Third number now equals:",num3)
