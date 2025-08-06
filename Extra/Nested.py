2511313554bf9586ac3083162dfee660259754b0
Abhi-Codingal <thisisabhimanyu2.0@gmail.com>
2025-08-06 14:23:33 -0500

    initial commit

---
+++ Extra/Nested.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+if age>20:
+    if age<50:
+        print("You are a good age for playing cricket.")
+    else:
+        print("You are too young")
+else:
+    print("You are too old.")
---
+++ Lesson 10/Calculator.py
@@ -0,0 +1,4 @@
+x=int(input("Enter a number: "))
+n=int(input("Enter the number you want to exponentiate to: "))
+for i in range(1,n+1):
+    print(x**i)
